dea9072d106959dc319039926674efbd32623066
MilosDrljaca <drljaca.milos1985@gmail.com>
2020-06-10 12:48:09 +0200

    Changed Book model, made new migration and created Create page in BookList.

--- BookStore/Migrations/20200609144502_AddISBN.Designer.cs
+++ BookStore/Migrations/20200609144502_AddISBN.Designer.cs
@@ -3,14 +3,16 @@ using BookStore.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BookStore.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class AplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200609144502_AddISBN")]
+    partial class AddISBN
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
