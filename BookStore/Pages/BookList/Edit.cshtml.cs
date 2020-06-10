41e1c1b80f60a820c3474d7000bcdece4d6b4359
MilosDrljaca <drljaca.milos1985@gmail.com>
2020-06-10 13:43:53 +0200

    Created Edit page.

---
+++ BookStore/Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookStore.Pages.BookList
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public async Task OnGet(int Id)
+        {
+            Book = await _db.Books.FindAsync(Id);
+        }
+    }
+}
