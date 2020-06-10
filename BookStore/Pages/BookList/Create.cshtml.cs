9d1398b4746298b772831d00a3c9cba6cb3619f1
MilosDrljaca <drljaca.milos1985@gmail.com>
2020-06-10 13:16:17 +0200

    Added validation for Create page, and created method OnPost.

--- BookStore/Pages/BookList/Create.cshtml.cs
+++ BookStore/Pages/BookList/Create.cshtml.cs
@@ -16,10 +16,25 @@ namespace BookStore.Pages.BookList
         {
             _db = db;
         }
+
+        [BindProperty]
         public Book Book { get; set; }
         public void OnGet()
         {
 
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.Books.AddAsync(Book);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
